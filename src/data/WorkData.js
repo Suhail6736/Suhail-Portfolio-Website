ce8d80afd44c7423840295da69b1e7e088870245
suhailkhan <155007233+Suhailsalmani@users.noreply.github.com>
2024-08-30 18:59:11 +0530

    New files

---
+++ src/data/WorkData.js
@@ -0,0 +1,33 @@
+// work data
+
+export const Work = [
+    {
+        id:1,
+        name:"Airbnb Project",
+        description:"Welcome to my Airbnb Clone, a full-stack application replicating Airbnb's core features, providing a seamless experience for hosts and guests with essential functionalities and user-friendly design",
+        tags:["Html","Css","Javascript","Node.js","Express","Mongodb","Cloudinary","Ejs"],
+        
+        demo:"https://suhail-airbnb.onrender.com/listings",
+        github:"#"
+    },
+    {
+        id:2,
+        name:"Shoping Web App (MERN)Stack",
+        description:"I recently built a fully responsive e-commerce website using the MERN stack. with awesome functionality",
+        tags:["React","Node.js","Express","Mongodb","Cloudinary"],
+        
+        demo:"https://shopping-app-suhail.netlify.app/",
+        github:"#"
+        
+    },
+    {
+        id:3,
+        name:"Project Fusion: A Javascript Showcase",
+        description:"Check out PROJECT FUSION where I've combined SIX cool JavaScript projects into one showcase, highlighting different and exciting features of JavaScript, HTML, and CSS.",
+        tags:["Html","Css","Javascript","gsap"],
+        
+        demo:"https://project-javascript-showcase.netlify.app/",
+        github:"#"
+    },
+    
+] 
